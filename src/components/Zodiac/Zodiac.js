287db8ffae107da4d9a27335224a93df1d9dd439
Charlie Hengemihle <charliehengemihle@gmail.com>
2023-01-10 16:33:56 -0700

    better css

--- src/components/Zodiac/Zodiac.js
+++ src/components/Zodiac/Zodiac.js
@@ -5,7 +5,7 @@ export default function Zodiac(props) {
     <div className="zodiac">
       <img alt={props.symbol} src={`${process.env.PUBLIC_URL}/signs/${props.symbol}.png`} />
       <span className="name">{props.name}</span>
-      <span>{props.dates}</span>
+      <span className="dates">{props.dates}</span>
     </div>
   );
 }
