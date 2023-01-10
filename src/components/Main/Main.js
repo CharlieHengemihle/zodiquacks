2dca84050261f787edd802b73fbfeb9858391f43
Charlie Hengemihle <charliehengemihle@gmail.com>
2023-01-10 15:03:10 -0700

    adding images and some other stuff I forgot

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import './Main.css';
+import Zodiac from '../Zodiac/Zodiac.js';
+import { zodiac } from '../../data.js';
+
 export default function Main() {
-  return <main></main>;
+  return (
+    <main>
+      {zodiac.map((sign) => (
+        <Zodiac key={sign.id} {...sign} />
+      ))}
+    </main>
+  );
 }
---
+++ src/components/Zodiac/Zodiac.js
@@ -0,0 +1,11 @@
+import './Zodiac.css';
+
+export default function Zodiac(props) {
+  return (
+    <div className="zodiac">
+      <img alt={props.symbol} src={`${process.env.PUBLIC_URL}/signs/${props.symbol}.png`} />
+      <span className="name">{props.name}</span>
+      <span>{props.dates}</span>
+    </div>
+  );
+}
