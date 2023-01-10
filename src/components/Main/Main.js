8ead2bbd5acf69d323db3c0b92eb108e33898a72
Charlie Hengemihle <charliehengemihle@gmail.com>
2023-01-10 15:15:01 -0700

    background image and css finishing touches

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './Main.css';
 import Zodiac from '../Zodiac/Zodiac.js';
 import { zodiac } from '../../data.js';
+import backgroundImg from '../../stars.jpg';
 
 export default function Main() {
   return (
-    <main>
+    <main style={{ backgroundImage: `url(${backgroundImg})` }}>
       {zodiac.map((sign) => (
         <Zodiac key={sign.id} {...sign} />
       ))}
